3dfeb9017bd2411f143dcb9150ae733d4647c0cc
thanhhung <thanhhung@tuoitre.com.vn>
2023-09-13 17:48:46 +0700

    [bugfix] fix bug click input "Giá trị hợp đồng" not show popconfirm on some browser: saphari, chrome

--- crm/src/components/contract/ContractValue.js
+++ crm/src/components/contract/ContractValue.js
@@ -1,97 +1,125 @@
-import { Popconfirm } from 'antd';
-import React, { useEffect, useState } from 'react'
+import { Popconfirm } from "antd";
+import React, { useEffect, useState } from "react";
 
 export default function ContractValue(props) {
+  const {
+    handleChangeValue,
+    valueOfField,
+    valueForm,
+    unlockInput,
+    setUnlockInput,
+    setValueForm,
+  } = props;
+  const [isEdit, setIsEdit] = useState(false);
 
-    const { handleChangeValue, valueOfField, valueForm, unlockInput, setUnlockInput, setValueForm } = props;
-    const [isEdit, setIsEdit] = useState(false)
+  useEffect(() => {
+    if (isEdit) {
+      let doanhThu = calculateDoanhThu("total_include_VAT");
+      setValueForm({
+        ...valueForm,
+        total_include_VAT: doanhThu,
+      });
+    }
+  }, [valueForm.VAT, valueForm.total, isEdit]);
 
-    useEffect(()=>{
-        if (isEdit) {
-            let doanhThu = calculateDoanhThu("total_include_VAT")
-            setValueForm({
-                ...valueForm,
-                total_include_VAT: doanhThu
-            })
-        }
-    }, [valueForm.VAT, valueForm.total, isEdit])
+  const calculateGTTH = (mode = "display") => {
+    if (valueForm.VAT === 10 || +valueForm.VAT === 10) {
+      return "";
+    } else {
+      let originalPrice = valueForm.total / 1.1;
+      let newGTTH = originalPrice * (1 + valueForm.VAT / 100);
+      return mode === "number"
+        ? newGTTH
+        : new Intl.NumberFormat("vi-VN", { maximumFractionDigits: 0 }).format(
+            newGTTH
+          );
+    }
+  };
 
-    const calculateGTTH = (mode = "display")=>{
-        if(valueForm.VAT === 10 || +valueForm.VAT === 10){
-            return ""
+  const calculateGiaSauChietKhau = (row) => {
+    if (row === 1) {
+      if (valueForm.discount_by_percent === 0) {
+        return new Intl.NumberFormat("vi-VN", {
+          maximumFractionDigits: 0,
+        }).format(valueForm.total);
+      } else {
+        let giaSauChietKhau =
+          valueForm.total -
+          (valueForm.total * valueForm.discount_by_percent) / 100;
+        return new Intl.NumberFormat("vi-VN", {
+          maximumFractionDigits: 0,
+        }).format(giaSauChietKhau);
+      }
+    } else if (row === 2) {
+      if (calculateGTTH("number") === "") {
+        return "";
+      } else {
+        let giaTriThucHien = calculateGTTH("number");
+        let giaSauChietKhau =
+          giaTriThucHien -
+          (giaTriThucHien * valueForm.discount_by_percent) / 100;
+        return new Intl.NumberFormat("vi-VN", {
+          maximumFractionDigits: 0,
+        }).format(giaSauChietKhau);
+      }
+    }
+  };
+  const calculateDoanhThu = (row) => {
+    if (row === 1) {
+      if (valueForm.total_include_VAT >= 0 && !isEdit) {
+        // console.log("line 45", valueForm.total_include_VAT)
+        return new Intl.NumberFormat("vi-VN").format(
+          valueForm.total_include_VAT * 1000000
+        );
+      } else {
+        let giaTriSauThue = valueForm.total / 1000000;
+        let VAT = valueForm.VAT;
+        let giaTruocThue = giaTriSauThue / 1.1;
+        // console.log("line 51")
+        if (giaTriSauThue > 0 && +VAT === 10) {
+          return new Intl.NumberFormat("vi-VN").format(valueForm.total);
         } else {
-            let originalPrice = valueForm.total / 1.1;
-            let newGTTH = originalPrice * (1 + (valueForm.VAT / 100));
-            return mode === "number" ? newGTTH : new Intl.NumberFormat("vi-VN", { maximumFractionDigits: 0 }).format(newGTTH);
+          let doanhThu = giaTruocThue + (giaTruocThue * VAT) / 100;
+          return new Intl.NumberFormat("vi-VN", {
+            maximumFractionDigits: 0,
+          }).format(doanhThu.toFixed(6) * 1000000);
         }
+      }
+    } else if (row === 2) {
+      let giaTriSauThue = valueForm.total / 1000000;
+      let VAT = Number(valueForm.VAT);
+      let discount = valueForm.discount_by_percent;
+      if (discount !== 0) {
+        if (giaTriSauThue > 0 && +VAT === 10) {
+          let doanhThuRow1 = giaTriSauThue;
+          let doanhThuRow2 = doanhThuRow1 - (doanhThuRow1 * discount) / 100;
+          return new Intl.NumberFormat("vi-VN").format(
+            doanhThuRow2.toFixed(6) * 1000000
+          );
+        } else {
+          let giaTruocThue = giaTriSauThue / 1.1;
+          let doanhThuRow1 = giaTruocThue + (giaTruocThue * VAT) / 100;
+          let doanhThuRow2 = doanhThuRow1 - (doanhThuRow1 * discount) / 100;
+          return new Intl.NumberFormat("vi-VN", {
+            maximumFractionDigits: 0,
+          }).format(doanhThuRow2.toFixed(6) * 1000000);
+        }
+      }
+      return "";
+    } else if (row === "total_include_VAT") {
+      let giaTriSauThue = valueForm.total / 1000000;
+      let VAT = valueForm.VAT;
+      let giaTruocThue = giaTriSauThue / 1.1;
+      if (giaTriSauThue > 0 && +VAT === 10) {
+        return valueForm.total / 1000000;
+      } else {
+        let doanhThu = giaTruocThue + (giaTruocThue * VAT) / 100;
+        return doanhThu.toFixed(6);
+      }
     }
+  };
 
-    const calculateGiaSauChietKhau = (row)=>{
-        if(row === 1){
-            if(valueForm.discount_by_percent === 0){
-                return new Intl.NumberFormat("vi-VN", { maximumFractionDigits: 0 }).format(valueForm.total);
-            } else {
-                let giaSauChietKhau = valueForm.total - (valueForm.total * valueForm.discount_by_percent / 100);
-                return new Intl.NumberFormat("vi-VN", { maximumFractionDigits: 0 }).format(giaSauChietKhau);
-            }
-        } else if(row === 2){
-            if(calculateGTTH("number") === ""){
-                return ""
-            } else {
-                let giaTriThucHien = calculateGTTH("number")
-                let giaSauChietKhau = giaTriThucHien - (giaTriThucHien * valueForm.discount_by_percent / 100);
-                return new Intl.NumberFormat("vi-VN", { maximumFractionDigits: 0 }).format(giaSauChietKhau);
-            }
-        }
-    }
-    const calculateDoanhThu = (row)=>{
-        if(row === 1){
-            if(valueForm.total_include_VAT >= 0  && !isEdit){
-                // console.log("line 45", valueForm.total_include_VAT)
-                return new Intl.NumberFormat("vi-VN").format(valueForm.total_include_VAT * 1000000);
-            } else {
-                let giaTriSauThue = valueForm.total / 1000000;
-                let VAT = valueForm.VAT;
-                let giaTruocThue = giaTriSauThue / 1.1;
-                // console.log("line 51")
-                if(giaTriSauThue > 0 && +VAT === 10){
-                    return new Intl.NumberFormat("vi-VN").format(valueForm.total)
-                } else {
-                    let doanhThu = giaTruocThue + (giaTruocThue * VAT / 100)
-                    return new Intl.NumberFormat("vi-VN", { maximumFractionDigits: 0 }).format(doanhThu.toFixed(6) * 1000000);
-                }
-            }
-        } else if(row ===2){
-            let giaTriSauThue = valueForm.total / 1000000;
-            let VAT = Number(valueForm.VAT);
-            let discount = valueForm.discount_by_percent;
-            if(discount !== 0){
-                if(giaTriSauThue > 0 && +VAT === 10){
-                    let doanhThuRow1 = giaTriSauThue;
-                    let doanhThuRow2 = doanhThuRow1 - (doanhThuRow1 * discount / 100)
-                    return new Intl.NumberFormat("vi-VN").format(doanhThuRow2.toFixed(6) * 1000000)
-                } else {
-                    let giaTruocThue = giaTriSauThue / 1.1;
-                    let doanhThuRow1 = giaTruocThue + (giaTruocThue * VAT / 100);
-                    let doanhThuRow2 = doanhThuRow1 - (doanhThuRow1 * discount / 100)
-                    return new Intl.NumberFormat("vi-VN", { maximumFractionDigits: 0 }).format(doanhThuRow2.toFixed(6) * 1000000);
-                }
-            }
-            return ""
-        } else if(row === "total_include_VAT") {
-            let giaTriSauThue = valueForm.total / 1000000;
-            let VAT = valueForm.VAT;
-            let giaTruocThue = giaTriSauThue / 1.1;
-            if(giaTriSauThue > 0 && +VAT === 10){
-                return valueForm.total / 1000000
-            } else {
-                let doanhThu = giaTruocThue + (giaTruocThue * VAT / 100) 
-                return doanhThu.toFixed(6)
-            }
-        }
-    }
-    
-    /** 
+  /** 
      * code cũ 4 ô input
      <div className="create__contract__value border_bottom_3px">
   <p>Giá trị hợp đồng</p>
@@ -161,101 +189,117 @@ export default function ContractValue(props) {
      */
 
   return (
-      <div className="create__contract__value border_bottom_3px">
-          <p>Giá trị hợp đồng</p>
-          <div className="field__input_3">
-              <div className="contract__field">
-                  <Popconfirm
-                      title="Bạn có muốn chỉnh sửa không?"
-                      onConfirm={() => { setUnlockInput(false); setIsEdit(true) }}
-                      // onCancel={cancel}
-                      okText="Có"
-                      cancelText="Không"
-                      disabled={!unlockInput}
-                  >
-                      <input
-                          className="style"
-                          type="text"
-                          name="total"
-                          onChange={(e) => {
-                              let { value, name } = e.target;
-                              let newValue = value.replaceAll(".", "");
-                              handleChangeValue(name, +newValue)
-                          }}
-                          value={valueOfField("total")}
-                          disabled={unlockInput}
-                      />
-                      <label className="pink__color">Giá trị sau thuế (10%)</label>
-                  </Popconfirm>
-              </div>
-              <div className="contract__field">
-                  <input className="style" type="text"
-                      name="VAT"
-                      disabled={window.location.href.includes("detail") ? true : false}
-                      onChange={(e) => {
-                          let { value, name } = e.target;
-                          handleChangeValue(name, value)
-                      }}
-                      value={valueOfField("VAT")}
-                  />
-                  <label>VAT</label>
-              </div>
-              <div className="contract__field">
-                  <input className="style" type="text"
-                      name="VAT"
-                      // onChange={(e) => {
-                      //   let { value, name } = e.target;
-                      //   handleChangeValue(name, +value)
-                      // }}
-                      value={calculateDoanhThu(1)}
-                      disabled
-                  />
-                  <label>Doanh thu</label>
-              </div>
-          </div>
-          <div className="field__input_3">
-              <div className="contract__field">
-                  <input className="style" type="text"
-                      name="discount_by_percent"
-                      // disabled
-                      onChange={(e) => {
-                          let { value, name } = e.target;
-                          if(value.includes(",")){
-                            handleChangeValue(name, value.replace(",", "."))
-                          } else if(value === ""){
-                            handleChangeValue(name, 0)
-                          } else {
-                            handleChangeValue(name, value)
-                          }
-                      }}
-                      value={valueOfField("discount_by_percent")}
-                  />
-                  <label>Chiết khấu (%)</label>
-              </div>
-              <div className="contract__field">
-                  <input className="style" type="text"
-                      name="VAT"
-                      // onChange={(e) => {
-                      //   let { value, name } = e.target;
-                      //   handleChangeValue(name, +value)
-                      // }}
-                      value={calculateDoanhThu(2)}
-                      disabled
-                  />
-                  <label>Doanh thu sau chiết khấu</label>
-              </div>
-          </div>
-          <div className="contract__value__note">
-              <textarea id="note"
-                  name="note"
-                  onChange={(e) => {
-                      let { value, name } = e.target;
-                      handleChangeValue(name, value)
-                  }}
-                  value={valueOfField("note")}
-              ></textarea>
-              <label>Ghi chú</label>
-          </div>
+    <div className="create__contract__value border_bottom_3px">
+      <p>Giá trị hợp đồng</p>
+      <div className="field__input_3">
+        <div className="contract__field">
+          <Popconfirm
+            title="Bạn có muốn chỉnh sửa không?"
+            onConfirm={() => {
+              setUnlockInput(false);
+              setIsEdit(true);
+            }}
+            // onCancel={cancel}
+            okText="Có"
+            cancelText="Không"
+            disabled={!unlockInput}
+          >
+            <input
+              style={{
+                backgroundColor: unlockInput ? "#fafafa" : "transparent",
+                cursor: unlockInput ? "default" : "",
+              }}
+              className="style"
+              type="text"
+              name="total"
+              onChange={(e) => {
+                let { value, name } = e.target;
+                let newValue = value.replaceAll(".", "");
+                handleChangeValue(name, +newValue);
+              }}
+              value={valueOfField("total")}
+              readOnly={unlockInput}
+            />
+            <label className="pink__color">Giá trị sau thuế (10%)</label>
+          </Popconfirm>
+        </div>
+        <div className="contract__field">
+          <input
+            className="style"
+            type="text"
+            name="VAT"
+            disabled={window.location.href.includes("detail") ? true : false}
+            onChange={(e) => {
+              let { value, name } = e.target;
+              handleChangeValue(name, value);
+            }}
+            value={valueOfField("VAT")}
+          />
+          <label>VAT</label>
+        </div>
+        <div className="contract__field">
+          <input
+            className="style"
+            type="text"
+            name="VAT"
+            // onChange={(e) => {
+            //   let { value, name } = e.target;
+            //   handleChangeValue(name, +value)
+            // }}
+            value={calculateDoanhThu(1)}
+            disabled
+          />
+          <label>Doanh thu</label>
+        </div>
       </div>
-  )
+      <div className="field__input_3">
+        <div className="contract__field">
+          <input
+            className="style"
+            type="text"
+            name="discount_by_percent"
+            // disabled
+            onChange={(e) => {
+              let { value, name } = e.target;
+              if (value.includes(",")) {
+                handleChangeValue(name, value.replace(",", "."));
+              } else if (value === "") {
+                handleChangeValue(name, 0);
+              } else {
+                handleChangeValue(name, value);
+              }
+            }}
+            value={valueOfField("discount_by_percent")}
+          />
+          <label>Chiết khấu (%)</label>
+        </div>
+        <div className="contract__field">
+          <input
+            className="style"
+            type="text"
+            name="VAT"
+            // onChange={(e) => {
+            //   let { value, name } = e.target;
+            //   handleChangeValue(name, +value)
+            // }}
+            value={calculateDoanhThu(2)}
+            disabled
+          />
+          <label>Doanh thu sau chiết khấu</label>
+        </div>
+      </div>
+      <div className="contract__value__note">
+        <textarea
+          id="note"
+          name="note"
+          onChange={(e) => {
+            let { value, name } = e.target;
+            handleChangeValue(name, value);
+          }}
+          value={valueOfField("note")}
+        ></textarea>
+        <label>Ghi chú</label>
+      </div>
+    </div>
+  );
 }
